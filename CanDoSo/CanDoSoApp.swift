009ccb056c1f77f47f44bfb71acbbbfe50da8b64
Reddy Lee <reddylee@gmail.com>
2021-12-24 23:30:50 +0000

    Initial Commit

---
+++ CanDoSo/CanDoSoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CanDoSoApp.swift
+//  CanDoSo
+//
+//  Created by 秦耕读 on 24/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct CanDoSoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
