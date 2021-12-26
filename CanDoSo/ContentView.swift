7bc05e73a3ee0c472dd0a961ce077470329a9d62
Reddy Lee <reddylee@gmail.com>
2021-12-26 22:46:24 +0000

    1226

--- CanDoSo/ContentView.swift
+++ CanDoSo/ContentView.swift
@@ -9,13 +9,29 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("欢迎光临耕读社!")
-            .padding()
+        VStack {
+            Spacer()
+            Image("R&S").resizable()
+                .scaledToFit()
+            Spacer()
+            Image(/*@START_MENU_TOKEN@*/"4sq"/*@END_MENU_TOKEN@*/)
+                .resizable()
+                .scaledToFit()
+            Spacer()
+            
+            VStack{
+                Text("Welcome to CanDoSo!").padding(.all)
+                Text("欢迎光临耕读社！")
+            }
+    }
+        
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 13 Pro")
     }
 }
